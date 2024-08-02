218d4b0f183919f0393b70382e0b2ce7cb5fdc13
youngsikkk <hys9805@younsei.ac.kr>
2024-08-02 15:09:18 +0900

    fix(Loading.jsx): class to className

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 const Loading = () => {
     return (
-        <div class="shadow rounded-md p-4 max-w-xl w-full mx-auto">
-            <div class="animate-pulse flex space-x-4">
-                <div class="flex-1 space-y-6 py-1">
-                    <div class="rounded-lg bg-slate-200 w-full h-70 lg:h-96 mx-auto"></div>
-                    <div class="h-2 bg-slate-200 rounded"></div>
-                    <div class="space-y-3">
-                        <div class="grid grid-cols-3 gap-4">
-                            <div class="h-2 bg-slate-200 rounded col-span-2"></div>
-                            <div class="h-2 bg-slate-200 rounded col-span-1"></div>
-                            <div class="h-2 bg-slate-200 rounded col-span-1"></div>
+        <div className="shadow rounded-md p-4 max-w-xl w-full mx-auto">
+            <div className="animate-pulse flex space-x-4">
+                <div className="flex-1 space-y-6 py-1">
+                    <div className="rounded-lg bg-slate-200 w-full h-70 lg:h-96 mx-auto"></div>
+                    <div className="h-2 bg-slate-200 rounded"></div>
+                    <div className="space-y-3">
+                        <div className="grid grid-cols-3 gap-4">
+                            <div className="h-2 bg-slate-200 rounded col-span-2"></div>
+                            <div className="h-2 bg-slate-200 rounded col-span-1"></div>
+                            <div className="h-2 bg-slate-200 rounded col-span-1"></div>
                         </div>
-                        <div class="h-2 bg-slate-200 rounded"></div>
+                        <div className="h-2 bg-slate-200 rounded"></div>
                     </div>
                 </div>
             </div>
