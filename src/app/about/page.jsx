b55903fd1574486016b91c3f72bfabbc2c725789
youngsikkk <hys9805@yonsei.ac.kr>
2024-08-05 17:39:57 +0900

    fix: change detail site

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const about = () => {
+    return (
+        <>
+            <h1>about</h1>
+        </>
+    )
+}
+
+export default about;
--- src/app/tech/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const TechHome = () => {
-    return (
-        <h1>Tech Home</h1>
-    )
-}
-
-export default TechHome;
