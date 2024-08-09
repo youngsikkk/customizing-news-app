516b05936de62366b3bc9fc5950bbc7a7f4ce7e4
youngsikkk <hys9805@yonsei.ac.kr>
2024-08-09 13:15:38 +0900

    design: navbar menu hover style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,12 +16,12 @@ const Header = () => {
             <div className="flex">
             <h1 className="text-3xl p-6 font-bold"><a href="/">Hello world</a></h1>
             <ul className="p-4 hidden lg:flex">
-                <li className="p-4 text-gray-600/60 font-bold"><a href="/">Home</a></li>
-                <li className="p-4 text-gray-600/60 font-bold"><a href="/mostpopular">Most Popular</a></li>
-                <li className="p-4 text-gray-600/60 font-bold"><a href="/archive">Archive</a></li>
-                <li className="p-4 text-gray-600/60 font-bold"><a href="/articlesearch">Article Search</a></li>
-                <li className="p-4 text-gray-600/60 font-bold"><a href="/books">Books</a></li>
-                <li className="p-4 text-gray-600/60 font-bold"><a href="/about">About</a></li>
+                <li className="p-4 text-gray-600/60 font-bold hover:text-blue-500 "><a href="/">Home</a></li>
+                <li className="p-4 text-gray-600/60 font-bold hover:text-blue-500"><a href="/mostpopular">Most Popular</a></li>
+                <li className="p-4 text-gray-600/60 font-bold hover:text-blue-500"><a href="/archive">Archive</a></li>
+                <li className="p-4 text-gray-600/60 font-bold hover:text-blue-500"><a href="/articlesearch">Article Search</a></li>
+                <li className="p-4 text-gray-600/60 font-bold hover:text-blue-500"><a href="/books">Books</a></li>
+                <li className="p-4 text-gray-600/60 font-bold hover:text-blue-500"><a href="/about">About</a></li>
             </ul>
             </div>
             <div className="flex">
