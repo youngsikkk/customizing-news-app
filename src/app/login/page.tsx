08cf622fec841b65a76653f734fa1d91cc5c7dd2
youngsikkk <hys9805@yonsei.ac.kr>
2024-08-23 13:35:34 +0900

    feat: sns login button

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,23 +1,34 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 
+// 아이콘을 사용하기 위해 react-icons 라이브러리를 사용합니다. 설치가 필요합니다.
+// npm install react-icons
+
+import { FaApple, FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+
 const LoginPage = () => {
-    
     const [id, setId] = useState("");
     const [password, setPassword] = useState("");
-    
+
+    const handleSubmit = (e:any) => {
+        e.preventDefault();
+        // 로그인 로직을 추가하세요
+    };
+
     return (
-        <div className="flex min-h-screen items-center justify-center ">
+        <div className="flex min-h-screen items-center justify-center">
             <div className="w-full max-w-md p-8 bg-white shadow-lg rounded-lg">
                 <h1 className="text-2xl font-bold mb-6 text-center">Login</h1>
-                <form className="space-y-4">
+                <form className="space-y-4" onSubmit={handleSubmit}>
                     <div>
                         <label htmlFor="username" className="block text-sm font-medium text-gray-700">Username</label>
                         <input
                             id="username"
                             type="text"
                             placeholder="Enter your username"
-                            onChange={(e) => {setId(e.target.value)}}
+                            value={id}
+                            onChange={(e) => setId(e.target.value)}
                             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                         />
                     </div>
@@ -27,7 +38,8 @@ const LoginPage = () => {
                             id="password"
                             type="password"
                             placeholder="Enter your password"
-                            onChange={(e) => {setPassword(e.target.value)}}
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                         />
                     </div>
@@ -38,6 +50,21 @@ const LoginPage = () => {
                         Log In
                     </button>
                 </form>
+                <div className="flex items-center justify-center"><p className="mt-4 mb-[-8px]">OR</p></div>
+                <div className="mt-6 space-y-4">
+                    <button className="w-full flex items-center justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-[#fff] hover:bg-[#357ae8] focus:outline-none focus:ring-2 focus:ring-blue-500">
+                        <FcGoogle className="mr-2 text-black" />
+                        Log in with Google
+                    </button>
+                    <button className="w-full flex items-center text-white justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-[#000000] hover:bg-[#333333] focus:outline-none focus:ring-2 focus:ring-gray-500">
+                        <FaApple className="mr-2 text-white" />
+                        Log in with Apple
+                    </button>
+                    <button className="w-full flex items-center justify-center py-2 px-4 border text-gray-300 border-gray-300 rounded-md shadow-sm bg-[#181717] hover:bg-[#333333] focus:outline-none focus:ring-2 focus:ring-gray-500">
+                        <FaGithub className="mr-2 text-white" />
+                        Log in with GitHub
+                    </button>
+                </div>
             </div>
         </div>
     );
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,22 +1,31 @@
-"use client"
+"use client";
 import React, { useState } from "react";
+import { FaGoogle, FaApple, FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 
 const SignUpPage = () => {
     const [id, setId] = useState("");
     const [password, setPassword] = useState("");
     const [passwordCheck, setPasswordCheck] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // 회원가입 로직을 추가하세요
+    };
+
     return (
-        <div className="flex min-h-screen items-center justify-center ">
+        <div className="flex min-h-screen items-center justify-center">
             <div className="w-full max-w-md p-8 bg-white shadow-lg rounded-lg">
                 <h1 className="text-2xl font-bold mb-6 text-center">Sign Up</h1>
-                <form className="space-y-4">
+                <form className="space-y-4" onSubmit={handleSubmit}>
                     <div>
                         <label htmlFor="username" className="block text-sm font-medium text-gray-700">Username</label>
                         <input
                             id="username"
                             type="text"
                             placeholder="Enter your username"
-                            onChange={(e) => {setId(e.target.value)}}
+                            value={id}
+                            onChange={(e) => setId(e.target.value)}
                             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                         />
                     </div>
@@ -25,18 +34,20 @@ const SignUpPage = () => {
                         <input
                             id="password"
                             type="password"
-                            onChange={(e) => {setPassword(e.target.value)}}
                             placeholder="Enter your password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                         />
                     </div>
                     <div>
-                        <label htmlFor="password check" className="block text-sm font-medium text-gray-700">Password Recheck</label>
+                        <label htmlFor="passwordCheck" className="block text-sm font-medium text-gray-700">Password Recheck</label>
                         <input
                             id="passwordCheck"
                             type="password"
-                            placeholder="Enter your password"
-                            onChange={(e) => {setPasswordCheck(e.target.value)}}
+                            placeholder="Re-enter your password"
+                            value={passwordCheck}
+                            onChange={(e) => setPasswordCheck(e.target.value)}
                             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                         />
                     </div>
@@ -47,9 +58,23 @@ const SignUpPage = () => {
                         Sign Up
                     </button>
                 </form>
+                <div className="mt-6 space-y-4">
+                    <button className="w-full flex items-center justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-[#fff] hover:bg-[#357ae8] focus:outline-none focus:ring-2 focus:ring-blue-500">
+                        <FcGoogle className="mr-2 text-black" />
+                        Log in with Google
+                    </button>
+                    <button className="w-full flex items-center text-white justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-[#000000] hover:bg-[#333333] focus:outline-none focus:ring-2 focus:ring-gray-500">
+                        <FaApple className="mr-2 text-white" />
+                        Log in with Apple
+                    </button>
+                    <button className="w-full flex items-center justify-center py-2 px-4 border text-gray-300 border-gray-300 rounded-md shadow-sm bg-[#181717] hover:bg-[#333333] focus:outline-none focus:ring-2 focus:ring-gray-500">
+                        <FaGithub className="mr-2 text-white" />
+                        Log in with GitHub
+                    </button>
+                </div>
             </div>
         </div>
     );
-}
+};
 
 export default SignUpPage;
