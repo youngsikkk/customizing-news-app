4acc2776fccc5df14e17b8ced82269793bf0b0e7
youngsikkk <hys9805@yonsei.ac.kr>
2024-08-23 11:52:49 +0900

    feat : sign up page structure

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,8 +2,47 @@ import React from "react";
 
 const SignUpPage = () => {
     return (
-        <h1>Sign Up Page</h1>
-    )
+        <div className="flex min-h-screen items-center justify-center ">
+            <div className="w-full max-w-md p-8 bg-white shadow-lg rounded-lg">
+                <h1 className="text-2xl font-bold mb-6 text-center">Sign Up</h1>
+                <form className="space-y-4">
+                    <div>
+                        <label htmlFor="username" className="block text-sm font-medium text-gray-700">Username</label>
+                        <input
+                            id="username"
+                            type="text"
+                            placeholder="Enter your username"
+                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+                        <input
+                            id="password"
+                            type="password"
+                            placeholder="Enter your password"
+                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password check" className="block text-sm font-medium text-gray-700">Password Recheck</label>
+                        <input
+                            id="passwordCheck"
+                            type="password"
+                            placeholder="Enter your password"
+                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="w-full bg-blue-500 text-white py-2 px-4 rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    >
+                        Sign Up
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default SignUpPage;
